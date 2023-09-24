bdb2e035c6837fd3329567c48eb1a408283e2d4b
AdrienneHarty <143745812+AdrienneHarty@users.noreply.github.com>
2023-09-24 18:09:08 -0400

    P2Lab2

---
+++ P2LAB2_HartyAdrienne.py
@@ -0,0 +1,10 @@
+num_1 = float(9184.25)
+num_2 = float(783.87)
+num_3 = float(6734.752)
+num_4 = float(4687.15)
+
+print("Number 1: ", f"{num_1:.3f}")
+print("Number 2: ", f"{num_2:.3f}")
+print("Number 3: ", f"{num_3:.3f}")
+print("Number 4: ", f"{num_4:.3f}")
+
